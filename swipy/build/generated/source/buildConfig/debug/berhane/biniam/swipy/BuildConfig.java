fb5584cb8873c7758cb99d34b570c6f94c2e805b
Biniam Haddish <benhaddish@gmail.com>
2019-06-29 16:21:44 +0300

    back to master

---
+++ swipy/build/generated/source/buildConfig/debug/berhane/biniam/swipy/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package berhane.biniam.swipy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "berhane.biniam.swipy";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "berhane.biniam.swipy";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
