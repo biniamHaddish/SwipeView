2db96deba918576969ee6c254a0adf9ec971d9c8
Biniam Haddish <benhaddish@gmail.com>
2019-06-30 16:54:15 +0300

    Java File to Kotlin

--- swipy/src/main/java/berhane/biniam/swipy/swipe/utils/RippleDirection.java
+++
@@ -1,6 +0,0 @@
-package berhane.biniam.swipy.swipe.utils;
-
-public enum RippleDirection{
-  END_TO_START,
-  START_TO_END,
-}
---
+++ swipy/src/main/java/berhane/biniam/swipy/swipe/utils/RippleDirection.kt
@@ -0,0 +1,6 @@
+package berhane.biniam.swipy.swipe.utils
+
+enum class RippleDirection {
+    END_TO_START,
+    START_TO_END
+}
